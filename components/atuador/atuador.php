525e9e7942739eec156f63c2cb874807f71700f8
MagiaSulphur <dave.jhd.dgc@gmail.com>
2024-04-08 20:31:16 +0100

    Sensor and atuador added

---
+++ components/atuador/atuador.php
@@ -0,0 +1,82 @@
+<div class="row g-4 mb-4">
+
+<div class="col-12 col-md-6 col-lg-3">
+                            <div class="card switchCard bg-body-tertiary border-0 d-flex align-items-center justify-content-between flex-row">
+                                <label class="switch">
+                                    <input class="cb" type="checkbox" />
+                                    <span class="toggle">
+                                        <span class="left">off</span>
+                                        <span class="right">on</span>
+                                    </span>
+                                </label>
+                                <div>
+                                    <h6 class="fw-bold fs-6 mb-0">Temperature</h6>
+                                    <div class="w-100 d-flex align-items-center justify-content-between">
+                                        <div class="d-flex flex-column">
+                                            <h5 class="card-title fs-1 fw-bold mb-0">20°</h5>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-12 col-md-6 col-lg-3">
+                            <div class="card switchCard bg-body-tertiary border-0 d-flex align-items-center justify-content-between flex-row">
+                                <label class="switch">
+                                    <input class="cb" type="checkbox" />
+                                    <span class="toggle">
+                                        <span class="left">off</span>
+                                        <span class="right">on</span>
+                                    </span>
+                                </label>
+                                <div>
+                                    <h6 class="fw-bold fs-6 mb-0">Humidity</h6>
+                                    <div class="w-100 d-flex align-items-center justify-content-between">
+                                        <div class="d-flex flex-column">
+                                            <h5 class="card-title fs-1 fw-bold mb-0">20°</h5>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-12 col-md-6 col-lg-3">
+                            <div class="card switchCard bg-body-tertiary border-0 d-flex align-items-center justify-content-between flex-row">
+                                <label class="switch">
+                                    <input class="cb" type="checkbox" />
+                                    <span class="toggle">
+                                        <span class="left">off</span>
+                                        <span class="right">on</span>
+                                    </span>
+                                </label>
+                                <div>
+                                    <h6 class="fw-bold fs-6 mb-0">Temperature</h6>
+                                    <div class="w-100 d-flex align-items-center justify-content-between">
+                                        <div class="d-flex flex-column">
+                                            <h5 class="card-title fs-1 fw-bold mb-0">20°</h5>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-12 col-md-6 col-lg-3">
+                            <div class="card switchCard bg-body-tertiary border-0 d-flex align-items-center justify-content-between flex-row">
+                                <label class="switch">
+                                    <input class="cb" type="checkbox" />
+                                    <span class="toggle">
+                                        <span class="left">off</span>
+                                        <span class="right">on</span>
+                                    </span>
+                                </label>
+                                <div>
+                                    <h6 class="fw-bold fs-6 mb-0">New Metric</h6>
+                                    <div class="w-100 d-flex align-items-center justify-content-between">
+                                        <div class="d-flex flex-column">
+                                            <h5 class="card-title fs-1 fw-bold mb-0">20°</h5>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        </div>
---
+++ components/sensor/sensor.php
@@ -0,0 +1,52 @@
+<div class="row g-4">
+                        <div class="col-12 col-md-6 col-lg-4">
+                            <div class="card dashboardCard bg-body-tertiary border-0 d-flex align-items-center justify-content-between flex-row">
+                                <div>
+                                    <h6 class="fw-bold fs-6 mb-0">Temperature</h6>
+                                    <div class="w-100 d-flex align-items-center justify-content-between">
+                                        <div class="d-flex flex-column">
+                                            <h5 class="card-title fs-1 fw-bold mb-0">20°</h5>
+                                            <p class="mb-0 fs-7 mainTextColor">11.38% Since last month</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <span class="bigIcon text-primary material-symbols-outlined">
+                                    ac_unit
+                                </span>
+                            </div>
+                        </div>
+
+                        <div class="col-12 col-md-6 col-lg-4">
+                            <div class="card dashboardCard bg-body-tertiary border-0 d-flex align-items-center justify-content-between flex-row">
+                                <div>
+                                    <h6 class="fw-bold fs-6 mb-0">Humidity</h6>
+                                    <div class="w-100 d-flex align-items-center justify-content-between">
+                                        <div class="d-flex flex-column">
+                                            <h5 class="card-title fs-1 fw-bold mb-0">20°</h5>
+                                            <p class="mb-0 fs-7 mainTextColor">11.38% Since last month</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <span class="bigIcon text-primary material-symbols-outlined">
+                                    humidity_mid
+                                </span>
+                            </div>
+                        </div>
+
+                        <div class="col-12 col-md-6 col-lg-4">
+                            <div class="card dashboardCard bg-body-tertiary border-0 d-flex align-items-center justify-content-between flex-row">
+                                <div>
+                                    <h6 class="fw-bold fs-6 mb-0">Temperatura</h6>
+                                    <div class="w-100 d-flex align-items-center justify-content-between">
+                                        <div class="d-flex flex-column">
+                                            <h5 class="card-title fs-1 fw-bold mb-0">20°</h5>
+                                            <p class="mb-0 fs-7 mainTextColor">11.38% Since last month</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <span class="bigIcon text-primary material-symbols-outlined">
+                                    thermometer
+                                </span>
+                            </div>
+                        </div>
+                    </div>
